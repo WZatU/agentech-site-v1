13320c43cc3389732d621589a70cda8ff71881cb
wesleyfan2015 <wesleyfan2015@gmail.com>
2026-06-26 15:04:42 -0700

    Use final account dashboard background

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,11 +1,9 @@
 import { AccountDashboard } from "@/components/account-dashboard";
-import { AccountMoon } from "@/components/account-moon";
 
 export default function AccountPage() {
   return (
     <main className="account-white-page account-illustrated-page min-h-screen bg-transparent px-5 py-10 text-slate-950 sm:px-6 lg:px-8 lg:py-14">
-      <AccountMoon />
-      <div className="relative z-[2] mx-auto max-w-7xl">
+      <div className="relative z-[2] mx-auto max-w-5xl">
         <AccountDashboard />
       </div>
     </main>
